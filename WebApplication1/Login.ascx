886289b60cd0d430f5f185ebecc764d500994a5c
Branko Rados <BRados@dspace.hr>
2021-12-16 18:52:32 +0100

    Add project files.

---
+++ WebApplication1/Login.ascx
@@ -0,0 +1,21 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Login.ascx.cs" Inherits="WebApplication1.WebUserControl1" %>
+<asp:Panel ID="PanelLogin" runat="server" Height="305px">
+    <h1>Welcome!<br /> Please enter username and password<br /></h1>
+    <br />
+    Username:&nbsp;&nbsp;
+    <asp:TextBox ID="username" runat="server" BorderStyle="None" ForeColor="Black" BackColor="#CCFF99"></asp:TextBox>
+    <br />
+    Password:&nbsp;&nbsp;
+    <asp:TextBox ID="password" runat="server" BorderStyle="None" ForeColor="Black" Height="21px" BackColor="#CCFF99"></asp:TextBox>
+    <br />
+    <br />
+    <br />
+    <asp:Button ID="Login" runat="server" Height="29px" Text="Login" OnClick="Login_Click" />
+</asp:Panel>
+<asp:Panel ID="PanelError" runat="server" style="margin-top: 58px" Visible="False">
+    <i>Username or password not valid</i></asp:Panel>
+<asp:Panel ID="PanelDisplay" runat="server" Height="154px" Width="981px" Visible="False">
+    <asp:GridView ID="GridViewData" runat="server">
+    </asp:GridView>
+</asp:Panel>
+
---
+++ WebApplication1/Login.ascx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace WebApplication1
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool CheckUsernameAndPassword()
+        {
+            XElement korisnici = XElement.Load(@"./App_Data/korisnici.xml");
+            var users = korisnici.Elements().Select(el => new
+                { username = (string)el.Element("korisnickoIme"), password = (string)el.Element("lozinka") });
+
+            if (!users.Any(user => user.username.Equals(this.username.Text)))
+                return false;
+
+            return users.Where(user => string.Compare(user.username, this.username.Text, true) == 0 && string.Compare(user.password, this.password.Text, true) == 0).Any();
+        }
+
+        private void DisplayBooks()
+        {
+            using (DataSet ds = new DataSet())
+            {
+                ds.ReadXml(@"./App_Data/popisKnjiga.xml")
+            }
+
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            if (CheckUsernameAndPassword())
+            {
+                this.PanelDisplay.Visible = true;
+            }
+            else
+            {
+                this.PanelLogin.Visible = false;
+                this.PanelError.Visible = true;
+                DisplayBooks();
+            }
+
+        }
+    }
+}
